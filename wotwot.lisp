fcb974ea429036d34e7bd0940292eb160f2b56e0
terminal625 <zpersoncool@gmail.com>
2018-11-17 00:16:05 -0500

    konz flips simplification

--- wotwot.lisp
+++ wotwot.lisp
@@ -7,20 +7,57 @@
 
 (defclass konz ()
   ((kar :initarg kar :initform nil)
-   (kdr :initarg kdr :initform nil)
    (flip :initarg flip :initform nil)))
 
+(defun konz (ze1 ze2)
+  (let ((a (make-instance 'konz 'kar ze1))
+	(d (make-instance 'konz 'kar ze2)))
+    (setf (slot-value a 'flip) d)
+    (setf (slot-value d 'flip) a)
+    a))
+
+(set-pprint-dispatch
+ 'konz
+ (lambda (stream object)
+   (write-char #\[ stream)
+   (write (kar object) :stream stream)
+   (write-char #\  stream)
+   (write (kdr object) :stream stream)
+   (write-char #\] stream)))
+
 (defmethod kar ((k konz))
-  (if (slot-value k 'flip)
-      (slot-value k 'kdr)
-      (slot-value k 'kar)))
+  (slot-value k 'kar))
+
+(defmethod (setf kar) (new (k konz))
+  (setf (slot-value k 'kar) new))
 
 (defmethod kdr ((k konz))
-  (if (slot-value k 'flip)
-      (slot-value k 'kar)
-      (slot-value k 'kdr)))
+  (slot-value (slot-value k 'flip) 'kar))
 
-(defun konz (ze1 ze2)
-  (make-instance 'konz 
-		 'kar ze1
-		 'kdr ze2))
+(defmethod (setf kar) (new (k konz))
+  (setf (slot-value (slot-value k 'flip) 'kar) new))
+
+(defun lizt (&rest args)
+  (labels ((rec (list)
+	     (if list
+		 (konz (car list)
+		       (rec (cdr list)))
+		 nil)))
+    (rec args)))
+
+(defun konvert-tree-to-kons (tree)
+  "convert a tree of cons cells into a kons tree. each list becomes a kons cell, 
+with the first and second element becoming the kar and kdr respectively"
+  (if tree
+      (if (consp tree)
+	  (konz (konvert-tree-to-kons (first tree))
+		(konvert-tree-to-kons (second tree)))
+	  tree)
+      nil))
+
+;;;(konvert-tree-to-kons '((234 234) (234 (234 234))))
+
+;;;1 1 -> 0
+;;;1 0 -> 1
+;;;0 1 -> 1
+;;;0 0 -> 0
